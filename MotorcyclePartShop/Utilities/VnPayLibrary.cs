d8fe5300a32f503f7bc7644d501eec77d3858920
ihatefish-sudo <trivd22@uef.edu.vn>
2026-04-04 00:29:22 +0700

    Ver 1.8

--- MotorcyclePartShop/Utilities/VnPayLibrary.cs
+++ MotorcyclePartShop/Utilities/VnPayLibrary.cs
@@ -77,33 +77,34 @@ namespace MotorcyclePartShop.Utilities
         }
 
         public class VnPayCompare : IComparer<string>
-    {
-        public int Compare(string x, string y)
         {
-            if (x == y) return 0;
-            if (x == null) return -1;
-            if (y == null) return 1;
-            var vnpCompare = CompareInfo.GetCompareInfo("en-US");
-            return vnpCompare.Compare(x, y, CompareOptions.Ordinal);
-        }
-    }
-
-    public static class Utils
-    {
-        public static string HmacSHA512(string key, string inputData)
-        {
-            var hash = new StringBuilder();
-            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
-            byte[] inputBytes = Encoding.UTF8.GetBytes(inputData);
-            using (var hmac = new HMACSHA512(keyBytes))
+            public int Compare(string x, string y)
             {
-                byte[] hashValue = hmac.ComputeHash(inputBytes);
-                foreach (var theByte in hashValue)
-                {
-                    hash.Append(theByte.ToString("x2"));
-                }
+                if (x == y) return 0;
+                if (x == null) return -1;
+                if (y == null) return 1;
+                var vnpCompare = CompareInfo.GetCompareInfo("en-US");
+                return vnpCompare.Compare(x, y, CompareOptions.Ordinal);
             }
-            return hash.ToString();
+        }
+
+        public static class Utils
+        {
+            public static string HmacSHA512(string key, string inputData)
+            {
+                var hash = new StringBuilder();
+                byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+                byte[] inputBytes = Encoding.UTF8.GetBytes(inputData);
+                using (var hmac = new HMACSHA512(keyBytes))
+                {
+                    byte[] hashValue = hmac.ComputeHash(inputBytes);
+                    foreach (var theByte in hashValue)
+                    {
+                        hash.Append(theByte.ToString("x2"));
+                    }
+                }
+                return hash.ToString();
+            }
         }
     }
 }
